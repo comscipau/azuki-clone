4f18aac263fd029012731529a13a084b6d3c384e
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2022-12-02 19:12:41 +0800

    animated mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { HiOutlineMenuAlt4 } from "react-icons/hi";
-import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { Squeeze as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -24,26 +23,31 @@ const Navbar = () => {
           <Link to="/world">WORLD</Link>
         </button>
       </div>
-      <div className="block md:hidden z-10" onClick={handleNav}>
-        {nav ? <AiOutlineClose size={32} /> : <HiOutlineMenuAlt4 size={32} />}
+      <div className="block md:hidden z-10">
+        {/* {nav ? <AiOutlineClose size={32} /> : <HiOutlineMenuAlt4 size={32} />} */}
+        <Hamburger toggled={nav} toggle={handleNav} />
       </div>
       <div
         className={
           nav
-            ? "fixed left-0 top-0 opacity-95 w-full h-full z-1 bg-[#d4d5d6] ease-in-out duration-500 block md:hidden"
-            : "fixed left-0 top-0 opacity-0 w-full h-full z-1 ease-in-out duration-500 hidden md:block"
+            ? "opacity-95 ease-in-out duration-500 fixed left-0 top-0 w-full h-full z-1 bg-[#d4d5d6] visible"
+            : "opacity-0 ease-in-out duration-500 fixed left-0 top-0 w-full h-full z-1 invisible"
         }
       >
         <ul className="ml-8 mr-14 mt-28">
-          <li
-            className="text-2xl font-bold py-4 border-b border-gray-400"
-            onClick={handleNav}
-          >
-            <Link to="/beanz">BEANZ</Link>
-          </li>
-          <li className="text-2xl font-bold py-4" onClick={handleNav}>
-            <Link to="/world">WORLD</Link>
-          </li>
+          <Link to="/beanz">
+            <li
+              className="text-2xl font-bold py-4 border-b border-gray-400"
+              onClick={handleNav}
+            >
+              BEANZ
+            </li>
+          </Link>
+          <Link to="/world">
+            <li className="text-2xl font-bold py-4" onClick={handleNav}>
+              WORLD
+            </li>
+          </Link>
         </ul>
       </div>
     </div>
